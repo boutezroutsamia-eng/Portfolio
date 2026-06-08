337e0b70c9138c22c0873098d0e51032ac121fd2
Samia Boutezrout <boutezroutsamia@gmail.com>
2026-06-08 16:40:28 +0200

    r

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useMemo, useState } from "react";
+import { Analytics } from "@vercel/analytics/next"
 import { AnimatePresence, motion } from "framer-motion";
 import {
   ArrowRight,
